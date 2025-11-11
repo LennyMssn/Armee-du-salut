6c6294641449c3341b688b3c1112fb1b447d0f00
LennyMssn <lennyptm@gmail.com>
2025-11-11 18:42:17 +0100

    update NAVs

--- block/nav2.php
+++ block/nav2.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark main-nav" style="background-color: rgba(0,0,0,0.2);">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/SAE/index.php">
-            <img src="/SAE/assets/images/Logo_de_l'Armée_du_Salut.png" alt="Logo" class="logo">
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/images/Logo_de_l'Armée_du_Salut.png" alt="Logo" class="logo">
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu"
@@ -11,11 +11,11 @@
 
         <div class="collapse navbar-collapse" id="navbarMenu">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-lg-center">
-                <li class="nav-item"><a class="nav-link" href="/SAE/actions.php">Actions sociales</a></li>
-                <li class="nav-item"><a class="nav-link" href="/SAE/actu.php">Actualités</a></li>
-                <li class="nav-item"><a class="nav-link" href="/SAE/rejoindre.php">Nous rejoindre</a></li>
-                <li class="nav-item"><a class="nav-link" href="/SAE/contact.php">Contact</a></li>
-                <li class="nav-item"><a class="btn btn-danger ms-lg-2" href="/SAE/Don.php">Don</a></li>
+                <li class="nav-item"><a class="nav-link" href="actions.php">Actions sociales</a></li>
+                <li class="nav-item"><a class="nav-link" href="actu.php">Actualités</a></li>
+                <li class="nav-item"><a class="nav-link" href="rejoindre.php">Nous rejoindre</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                <li class="nav-item"><a class="btn btn-danger ms-lg-2" href="Don.php">Don</a></li>
 
                 <li class="nav-item dropdown ms-lg-3">
                     <button class="btn btn-outline-light dropdown-toggle"
